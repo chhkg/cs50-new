0b5a313fda6c72ad510f1f20bb5ea2b1c89602f9
chhkg <chhkg@users.noreply.github.com>
2020-03-15 11:35:15 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -5,14 +5,15 @@
 int main(void)
 {
     float change;
-    do{
+    do
+    {
         change = get_float("Change owed: ");
     }
     while (change < 0);
     int c = round(change * 100);
-    int q = c/25;
-    int d = c%25/10;
-    int n = c%25%10/5;
-    int p = c%25%10%5;
+    int q = c / 25;
+    int d = c % 25 / 10;
+    int n = c % 25 % 10 / 5;
+    int p = c % 25 % 10 % 5;
     printf("%i\n", q + d + n + p);
 }

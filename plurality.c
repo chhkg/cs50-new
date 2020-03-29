c03fdcb2451e5a7479e2ba20e44229abd4a99d14
chhkg <chhkg@users.noreply.github.com>
2020-03-29 16:39:40 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -72,8 +72,11 @@ bool vote(string name)
         if (strcmp(name, candidates[j].name) == 0)
         {
             candidates[j].votes++;
-//            printf("%s has %i votes\n", candidates[j].name, candidates[j].votes);
             return true;
+        }
+        else
+        {
+            return false;
         }
     }
     return false;

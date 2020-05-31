6b728f423dec33cab8290ffff132684ee27373ab
chhkg <chhkg@users.noreply.github.com>
2020-05-31 17:01:19 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -1,11 +1,12 @@
 from cs50 import get_string
 
+
 def main():
     text = get_string("Text: ")
 
-    lc = count_letters(text);
-    wc = count_words(text);
-    sc = count_sentences(text);
+    lc = count_letters(text)
+    wc = count_words(text)
+    sc = count_sentences(text)
 
     index = 0.0588 * (lc / wc) * 100 - 0.296 * (sc / wc) * 100 - 15.8
 
@@ -16,12 +17,14 @@ def main():
     else:
         print(f"Grade {round(index)}")
 
+
 def count_letters(paragraph):
     letter_c = 0
     for i in range(len(paragraph)):
         if (paragraph[i].isalpha()):
             letter_c += 1
     return letter_c
+
 
 def count_words(paragraph):
     word_c = 1
@@ -30,11 +33,13 @@ def count_words(paragraph):
             word_c += 1
     return word_c
 
+
 def count_sentences(paragraph):
     sentence_c = 0
     for i in range(len(paragraph)):
         if paragraph[i] in (".", "?", "!"):
             sentence_c += 1
     return sentence_c
+
 
 main()
